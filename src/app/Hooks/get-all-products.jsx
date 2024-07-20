b2b6b0da6c09d73298e18b108c192f7d50eda9dd
mohamedabdo111 <moabdo346@gmail.com>
2024-07-21 02:59:03 +0300

    first project

---
+++ src/app/Hooks/get-all-products.jsx
@@ -0,0 +1,11 @@
+const GetAllProducts = async () => {
+  const res = await fetch("https://dummyjson.com/products");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+
+export default GetAllProducts;
---
+++ src/app/products/[id]/page.jsx
@@ -0,0 +1,10 @@
+import GetAllProducts from "@/app/Hooks/get-all-products";
+import React from "react";
+
+const ProductsID = async ({ params }) => {
+  //   const dataif = await GetAllProducts();
+  //   console.log(data);
+  return <div>details</div>;
+};
+
+export default ProductsID;
